6d719cc2676bf1cd0b610ea1aff9f1f69aae73bb
Chantalice <chantal.rakotoarivelo2020@gmail.com>
2020-10-28 17:00:47 +0100

    Dépot pokedex

---
+++ pokedex.js
@@ -0,0 +1,45 @@
+let pokemonId = 1
+let apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+function getData(id) {
+    apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+    fetch(apiUrl)
+    .then( (data) => data.json())
+    .then( pokemon => generateHtml(pokemon))
+}
+getData();
+
+function pokeSlideRight() {
+  pokemonId ++;
+  getData(pokemonId);
+}
+
+pokeSlideRight();
+
+function pokeSlideLeft() {
+  pokemonId --;
+  getData(pokemonId);
+}
+
+pokeSlideLeft();
+
+const generateHtml = (data) => {
+    console.log(data)
+    const html = `
+            <div class="name">${data.name}</div>
+            <div class = "name">Id : ${data.id}</div>
+            <img src=${data.sprites.front_default} height="250">
+            <div class="details">
+            <span>Taille: ${data.height}</span>
+            <span>Poids: ${data.weight}</span>
+            <span>Type: ${data.types[0].type.name}</span>
+            <span>Vies: ${data.stats[0].base_stat}</span>
+            <span>Attaque: ${data.stats[1].base_stat}</span>
+            <span>Défense: ${data.stats[2].base_stat}</span>
+            <span>Attaque spéciale: ${data.stats[3].base_stat}</span>
+            <span>Défense spéciale: ${data.stats[4].base_stat}</span>
+            </div>
+    `
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html
+}
