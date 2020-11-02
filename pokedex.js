19fd3f7d32e458c1bdb79a979b7fa2c238ffa505
Chantalice <chantal.rakotoarivelo2020@gmail.com>
2020-11-02 14:34:17 +0100

    mise à jour dépôt

--- pokedex.js
+++ pokedex.js
@@ -26,12 +26,11 @@ pokeSlideLeft();
 const generateHtml = (data) => {
     console.log(data)
     const html = `
-            <div class="name">${data.name}</div>
-            <div class = "name">Id : ${data.id}</div>
+            <div id="left"></div>
+            <div class="name"><p>${data.name}<br>Id: ${data.id}</p></div>
             <img src=${data.sprites.front_default} height="250">
             <div class="details">
-            <span>Taille: ${data.height}</span>
-            <span>Poids: ${data.weight}</span>
+            <span>Taille: ${data.height} // Poids: ${data.weight}</span>
             <span>Type: ${data.types[0].type.name}</span>
             <span>Vies: ${data.stats[0].base_stat}</span>
             <span>Attaque: ${data.stats[1].base_stat}</span>
@@ -39,6 +38,7 @@ const generateHtml = (data) => {
             <span>Attaque spéciale: ${data.stats[3].base_stat}</span>
             <span>Défense spéciale: ${data.stats[4].base_stat}</span>
             </div>
+            <div id="right"></div>
     `
     const pokemonDiv = document.querySelector('.pokemon')
     pokemonDiv.innerHTML = html
